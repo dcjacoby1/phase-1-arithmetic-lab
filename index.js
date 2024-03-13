83eb431e6f1942b3aabb66871827a841b0e1edd5
Dan Jacoby <jacobydan01@gmail.com>
2024-03-12 22:36:08 -0400

    done

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+const num3 = 28
+const num4 = 8
+let multiply = num1 * num2
+let random = Math.floor(Math.random()*100) + 1;
+let mod = num3 % num4;
+let max = Math.max(1,20);
